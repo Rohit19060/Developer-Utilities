61e19298d071dd7d7db13be17823207dcc152793
Rohit Jain <rohitjain19060@gmail.com>
2021-10-14 07:42:00 +0530

    :crown: Updated Postman Tests

--- Postman_Tests.js
+++ Postman_Tests.js
@@ -1,12 +1,60 @@
 pm.test("Status code is 201", () => {
-    pm.response.to.have.status(201);
-    pm.response.to.be.success;
-    pm.response.to.be.json;
-    pm.expect(pm.response.code).to.equal(201);
+    pm.response.to.have.status(200);
+    pm.expect(pm.response.code).to.equal(200);
+    pm.expect(pm.response.code).to.be.oneOf([201, 200]);
+    pm.response.to.be.ok;
 });
 
-pm.test("Message", () => {
-    pm.expect(true, "No why fail").to.be.ok;
+pm.test("Response time is less than 200ms", () => {
+    pm.expect(pm.response.responseTime).to.be.below(250);
+});
+
+pm.test("Content type is present", () => {
+    pm.response.to.have.header("Content-Type");
+    pm.expect(pm.response.headers.get("Content-Type")).to.be.equal(
+        "application/json"
+    );
+    pm.response.to.be.json;
+});
+
+pm.test("Test data type of the response", () => {
+    const res = pm.response.json();
+    pm.expect(res[0]).to.be.an("object");
+    pm.expect(res[0].name).to.be.a("string");
+    pm.expect(res[0].id).to.be.a("number");
+    pm.expect(res).to.be.an("array");
+    pm.expect(res[0].website).to.be.undefined;
+    pm.expect(res[0].email).to.be.undefined;
+    // pm.expect(res[0].email).to.be.null;
+    pm.expect(res).to.be.ok;
+});
+
+pm.test("Response body should be an array", () => {
+    let res = pm.response.json();
+    pm.expect(res).to.be.an("array");
+    pm.expect(res).to.have.length.above(0);
+});
+
+pm.test("Response should have a name & id property", () => {
+    let res = pm.response.json();
+    pm.expect(res[0]).to.have.property("name");
+    pm.expect(pm.response.text()).to.include("id");
+});
+
+pm.test("Response body should not return password", () => {
+    let res = pm.response.json();
+    pm.expect(res).to.not.include("password");
+    pm.expect(res).to.not.have.property("password");
+});
+
+let res = pm.response.json();
+pm.environment.set("id", res[0]["id"]);
+pm.collectionVariables.set("id", res[0].id);
+pm.globals.set("id", res[0]["id"]);
+console.log(pm.environment.get("id"));
+
+pm.test("Check the active environment", () => {
+    pm.expect(pm.environment.name).to.eql("Api-Template");
 });
 
 var schema = {
@@ -22,101 +70,6 @@ pm.test("Schema is valid", () => {
     pm.expect(tv4.validate(data1, schema)).to.be.true;
 });
 
-pm.test("Response time is less thaan 200ms", () => {
-    pm.expect(pm.response.responseTime).to.be.below(250);
-});
-
-pm.test("Content type is json", () => {
-    pm.response.to.be.json;
-});
-
-pm.test("Content type is JSON", () => {
-    pm.response.to.have.header("Content-Type");
-});
-
-let jsonresponse = pm.response.json();
-
-pm.test("Response should be a parsable json", () => {
-    pm.expect(jsonresponse).to.be.ok;
-});
-
-pm.test("Response body should be an array", () => {
-    console.log(jsonresponse);
-    pm.expect(jsonresponse).to.be.a("Array");
-});
-
-pm.test("Does not return password", () => {
-    pm.expect(jsonresponse).to.not.include("password");
-});
-
-pm.environment.set("id", jsonresponse[0]["id"]);
-pm.globals.set("exampleLocalVariable", "this-example-of-a-local-variable");
-
-console.log(pm.environment.get("variable_key"));
-console.log(pm.globals.get("variable_key"));
-console.log(pm.variables.get("variable_key"));
-console.log(pm.collectionVariables.get("variable_key"));
-console.log(pm.environment.set("variable_key", "variable_value"));
-
-pm.test("Item name is correct", function () {
-    let response = pm.response.json();
-    let savedItemName = pm.environment.get("bitcoinRate");
-    pm.expect(response).to.be.equal(savedItemName);
-});
-
-pm.test("Cart does not include deleted item", function () {
-    let savedItemId = pm.environment.get("itemId");
-    pm.response.to.not.include(savedItemId);
-});
-
-pm.test("Body Contain String", () => {
-    pm.expect(pm.response.text()).to.include("id");
-});
-
-pm.test("status code as array", () => {
-    pm.expect(pm.response.code).to.be.oneOf([201, 200]);
-});
-
-pm.test("Check the active environment", () => {
-    pm.expect(pm.environment.name).to.eql("Api-Template");
-});
-
-pm.test("Content-Type header is application/json", () => {
-    pm.expect(pm.response.headers.get("Content-Type")).to.eql("application/json");
-});
-
-pm.test("Response property matches environment variable", function () {
-    pm.expect(pm.response.json().name).to.eql(pm.environment.get("name"));
-});
-
-/* response has this structure:
-{
-  "name": "Jane",
-  "age": 29,
-  "hobbies": [
-    "skating",
-    "painting"
-  ],
-  "email": null
-}
-*/
-const jsonData = pm.response.json();
-pm.test("Test data type of the response", () => {
-    pm.expect(jsonData).to.be.an("object");
-    pm.expect(jsonData.name).to.be.a("string");
-    pm.expect(jsonData.age).to.be.a("number");
-    pm.expect(jsonData.hobbies).to.be.an("array");
-    pm.expect(jsonData.website).to.be.undefined;
-    pm.expect(jsonData.email).to.be.null;
-});
-
-
 pm.sendRequest("https://postman-echo.com/get", function (err, response) {
     console.log(response.json());
 });
-
-// Prerequest Script
-var uuid = require("uuid");
-var id = uuid.v4();
-var email = `testuser-${id.slice(id.length - 6)}@unbreakableapi.com`;
-pm.collectionVariables.set("userEmail", email);
